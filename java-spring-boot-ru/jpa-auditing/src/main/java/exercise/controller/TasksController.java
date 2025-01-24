834109e0f91de43070ad7ff712470c0b684a1a21
@hexlet/cli <support@hexlet.io>
2025-01-24 12:10:17 +0300

    submit java-spring-boot-ru/jpa-auditing

---
+++ java-spring-boot-ru/jpa-auditing/src/main/java/exercise/controller/TasksController.java
@@ -0,0 +1,71 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+import exercise.model.Task;
+import exercise.repository.TaskRepository;
+import exercise.exception.ResourceNotFoundException;
+
+@RestController
+@RequestMapping("/tasks")
+public class TasksController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping(path = "")
+    public List<Task> index() {
+        return taskRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Task show(@PathVariable long id) {
+
+        var task =  taskRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException(getNotFoundMessage(id)));
+
+        return task;
+    }
+
+    private static String getNotFoundMessage(long id) {
+        return "Task with id " + id + " not found";
+    }
+
+    // BEGIN
+   /* POST /tasks — создание новой задачи
+    PUT /tasks/{id} — обновление задачи*/
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task create(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Task update(@PathVariable Long id,  @RequestBody Task task) {
+        Task found = taskRepository.findById(id).orElseThrow(()->new ResourceNotFoundException(getNotFoundMessage(id)));
+        found.setTitle(task.getTitle());
+        found.setDescription(task.getDescription());
+        taskRepository.save(found);
+        return found;
+    }
+    // END
+
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable long id) {
+        taskRepository.deleteById(id);
+    }
+}
